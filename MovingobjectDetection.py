f100b3992ff4b4fae1a0d345bd48028178fd70cb
Anbalan777 <Anbalan777@gmail.com>
2023-08-07 12:43:59 +0530

    MOD

---
+++ MovingobjectDetection.py
@@ -0,0 +1,36 @@
+import cv2
+import time
+import imutils
+
+vs=cv2.VideoCapture("Manfrotto Move.mp4")
+time.sleep(1)
+firstFrame=None
+area=1000
+while True:
+    _,img=vs.read()
+    text="Normal"
+    img=imutils.resize(img,width=800)
+    grayImg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    blur=cv2.GaussianBlur(grayImg,(21,21),0)
+    if firstFrame is None:
+        firstFrame=blur
+        continue
+    imgDiff=cv2.absdiff(firstFrame,blur)
+    thresholdImg=cv2.threshold(imgDiff,25,255,cv2.THRESH_BINARY)[1]
+    thresholdImg=cv2.dilate(thresholdImg,None,iterations=2)
+    cnts=cv2.findContours(thresholdImg.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts=imutils.grab_contours(cnts)
+    for c in cnts :
+        if cv2.contourArea(c) < area:
+            continue
+        (x,y,w,h)=cv2.boundingRect(c)
+        cv2.rectangle(img,(x,y),((x+w),(y+h)),(0,255,0),2)
+        text="MovingObjectDetection"
+    print(text)
+    cv2.putText(img, text, (10, 20),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.imshow("VidoeStream",img)
+    if cv2.waitKey(10) &0xFF == 27:
+        break
+vs.release()
+cv2.destroyAllWindows()    
